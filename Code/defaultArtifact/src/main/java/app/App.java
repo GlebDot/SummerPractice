93b1d0545825e9d3bb974c74699d4e3ba245401b
holyunchained <42898121+holyunchained@users.noreply.github.com>
2020-07-02 21:59:30 +0300

    Add files via upload

--- Code/defaultArtifact/src/main/java/app/App.java
+++ Code/defaultArtifact/src/main/java/app/App.java
@@ -1,11 +1,14 @@
 package app;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
@@ -23,60 +26,38 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
         Border defaultBorder = new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
         new CornerRadii(1), new BorderWidths(2)));
 
-        Button fileEditButton = new Button("File input"); //upload graph from file
-        fileEditButton.setLayoutX(170.0);
-        fileEditButton.setLayoutY(50.0);
-        fileEditButton.setPrefWidth(100.0);
-        fileEditButton.setPrefHeight(50.0);
-
-        Button clearGraphButton = new Button("Delete graph");
-        clearGraphButton.setLayoutX(290.0);
-        clearGraphButton.setLayoutY(50.0);
-        clearGraphButton.setPrefWidth(100.0);
-        clearGraphButton.setPrefHeight(50.0);
-
-        Button onWatchModeButton = new Button("Watch"); //change mode to watch (and back to edit)
-        onWatchModeButton.setLayoutX(50.0);
-        onWatchModeButton.setLayoutY(50.0);
-        onWatchModeButton.setPrefWidth(100.0);
-        onWatchModeButton.setPrefHeight(50.0);
-
-        Button makeAlgStepButton = new Button("1 Step");
-        makeAlgStepButton.setLayoutX(610.0);
-        makeAlgStepButton.setLayoutY(65.0);
-        makeAlgStepButton.setPrefWidth(70.0);
-        makeAlgStepButton.setPrefHeight(35.0);
+        Button fileEditButton = setButtonConfiguration(170.0, 50.0, 100.0, 50.0, "File input");
+        Button clearGraphButton = setButtonConfiguration(290.0, 50.0, 100.0, 50.0, "Delete graph");
+        Button onWatchModeButton = setButtonConfiguration(50.0, 50.0, 100.0, 50.0, "Watch");
+        Button makeAlgStepButton = setButtonConfiguration(610.0, 65.0, 70.0, 35.0, "1 Step");
+        Button runFullAlgButton = setButtonConfiguration(700.0, 65.0, 70.0, 35.0, "Run full");
+        Button reRunAlgButton = setButtonConfiguration(790.0, 65.0, 70.0, 35.0, "Re run");
+        reRunAlgButton.setVisible(false);
         makeAlgStepButton.setVisible(false);
-
-        Button runFullAlgButton = new Button("Run full");
-        runFullAlgButton.setLayoutX(700.0);
-        runFullAlgButton.setLayoutY(65.0);
-        runFullAlgButton.setPrefWidth(70.0);
-        runFullAlgButton.setPrefHeight(35.0);
         runFullAlgButton.setVisible(false);
 
-        Button reRunAlgButton = new Button("Re run");
-        reRunAlgButton.setLayoutX(790.0);
-        reRunAlgButton.setLayoutY(65.0);
-        reRunAlgButton.setPrefWidth(70.0);
-        reRunAlgButton.setPrefHeight(35.0);
-        reRunAlgButton.setVisible(false);
+        Label loggerLabel = new Label("Edit Logger:");
+        loggerLabel.setPrefWidth(200);
+        loggerLabel.setLayoutX(1000.0);
+        loggerLabel.setLayoutY(120.0);
+        loggerLabel.setBorder(defaultBorder);
 
-
-        Label editLogger = new Label("Edit Logger:"); //in fact just loggerButton, label is temporary
-        editLogger.setPrefWidth(200);
-        editLogger.setLayoutX(1000.0);
-        editLogger.setLayoutY(120.0);
+        ObservableList<String> strList = FXCollections.observableArrayList("Edit history:");
+        ListView<String> loggerTable = new ListView<String>(strList);
+        loggerTable.setPrefWidth(200);
+        loggerTable.setPrefHeight(580);
+        loggerTable.setLayoutX(1000.0);
+        loggerTable.setLayoutY(140.0);
+        loggerTable.setBorder(defaultBorder);
 
         HBox graphEditorBox = new HBox();
         graphEditorBox.setBorder(defaultBorder);
         graphEditorBox.setLayoutX(50);
         graphEditorBox.setLayoutY(120);
-        graphEditorBox.setPrefHeight(700);
+        graphEditorBox.setPrefHeight(600);
         graphEditorBox.setPrefWidth(900);
 
         //handlers
@@ -86,25 +67,25 @@ public class App extends Application {
                 FileChooser fileChooser = new FileChooser();
                 fileChooser.setTitle("Open Resource File");
                 fileChooser.showOpenDialog(stage);
-                String str = editLogger.getText() + "\nInput edit pressed";
-                editLogger.setText(str);
             }
         });
 
         clearGraphButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String str = "Edit Logger:\nGraph cleared";
-                editLogger.setText(str);
+                strList.clear();
+                strList.add("Graph cleared");
             }
         });
 
-        onWatchModeButton.setOnAction(new EventHandler<ActionEvent>() { //not ended yet
+        onWatchModeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 if(onWatchModeButton.getText() == "Watch"){
                     String str = "Algorithm Logger:";
-                    editLogger.setText(str);
+                    strList.clear();
+                    strList.add("Algorithm steps:");
+                    loggerLabel.setText(str);
                     fileEditButton.setDisable(true);
                     clearGraphButton.setDisable(true);
                     onWatchModeButton.setText("Back to edit");
@@ -113,7 +94,9 @@ public class App extends Application {
                     reRunAlgButton.setVisible(true);
                 }
                 else{
-                    editLogger.setText("Edit Logger:");
+                    loggerLabel.setText("Edit Logger:");
+                    strList.clear();
+                    strList.add("Edit history:");
                     fileEditButton.setDisable(false);
                     clearGraphButton.setDisable(false);
                     onWatchModeButton.setText("Watch");
@@ -127,37 +110,48 @@ public class App extends Application {
         makeAlgStepButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String str = editLogger.getText() + "\nMaking step...";
-                editLogger.setText(str);
+                strList.add("Making step...");
             }
         });
 
         runFullAlgButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String str = editLogger.getText() + "\nRun full";
-                editLogger.setText(str);
+                strList.add("Run full");
             }
         });
 
         reRunAlgButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String str = editLogger.getText() + "\nRe run full";
-                editLogger.setText(str);
+                strList.add("start from the beginning");
             }
         });
 
-        Group root = new Group(editLogger, fileEditButton, clearGraphButton, onWatchModeButton,
-                makeAlgStepButton, runFullAlgButton, reRunAlgButton, graphEditorBox);
+        Group root = new Group(loggerLabel, fileEditButton, clearGraphButton, onWatchModeButton,
+                makeAlgStepButton, runFullAlgButton, reRunAlgButton, graphEditorBox, loggerTable);
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
         stage.setTitle("Everlasting summer practice");
+        //scene.setFill(Color.GRAY);
         stage.setWidth(1250);
         stage.setHeight(800);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+    }
+
+    private Button setButtonConfiguration(double offsetX, double offsetY, double width, double height, String text){
+        Border defaultBorder = new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+                new CornerRadii(1), new BorderWidths(2)));
+        Button button = new Button(text);
+        button.setLayoutX(offsetX);
+        button.setLayoutY(offsetY);
+        button.setPrefWidth(width);
+        button.setPrefHeight(height);
+        button.setBorder(defaultBorder);
+        return button;
     }
 
     public static void main(String[] args) {
