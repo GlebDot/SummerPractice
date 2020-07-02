56d85ab80185562b0469ff568ac68b44d4b6d63c
GlebDot <bessudnov.gleb@gmail.com>
2020-07-02 12:57:51 +0300

    Revert "Merge pull request #2 from GlebDot/Protorype_1"
    This reverts commit 34cfacd88f79dd2fe8599cab4036e46684084942, reversing
    changes made to 77ca3491e0237ac412e04ad9b5a682fb8a370c78.

--- Code/defaultArtifact/src/main/java/graph/IGraph.java
+++ Code/defaultArtifact/src/main/java/graph/IGraph.java
@@ -4,6 +4,4 @@ public interface IGraph {
     public abstract IGraph clone();
     public abstract void addVertex(Vertex v);
     public abstract void addEdge(Edge e);
-    public abstract void deleteEdge(Edge e);
-    public abstract void deleteVertex(Vertex v);
 }
