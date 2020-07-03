71105199913e413edab1612d720d54ebe7c26861
Pchooo <42907700+Pchooo@users.noreply.github.com>
2020-07-03 18:09:02 +0500

    Update Vertex.java

--- Code/defaultArtifact/src/main/java/graph/Vertex.java
+++ Code/defaultArtifact/src/main/java/graph/Vertex.java
@@ -3,9 +3,17 @@ package graph;
 /**class for storing information about vertex of the graph */
 public class Vertex {
     int number;
+    int distanse;
+    boolean isStart;
+    boolean isCheck;
+
 
     public Vertex(){
         number = -1;
+        distanse = 0;
+        isStart = false;
+        isCheck = false; 
+
     }
 
     public void setNumber(int number){
