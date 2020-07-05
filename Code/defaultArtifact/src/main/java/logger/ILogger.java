3cecc9a34550e7e81d8eb6cce388b9f444b1b152
holyunchained <42898121+holyunchained@users.noreply.github.com>
2020-07-05 12:49:17 +0300

    Logger interface changing

--- Code/defaultArtifact/src/main/java/logger/ILogger.java
+++ Code/defaultArtifact/src/main/java/logger/ILogger.java
@@ -1,7 +1,11 @@
 package logger;
 
+import javafx.collections.ObservableList;
+
 public interface ILogger {
     public abstract void logEvent(AlgorithmMessage message);
+
+    public abstract void logEvent(String message);
 
     public abstract void clear();
 }
