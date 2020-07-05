cf74e39214cf9c9bc4ae0a3ff4d4062f8e373629
GlebDot <bessudnov.gleb@gmail.com>
2020-07-05 14:03:11 +0300

    Update GraphEditor.java

--- Code/defaultArtifact/src/main/java/graphEditor/GraphEditor.java
+++ Code/defaultArtifact/src/main/java/graphEditor/GraphEditor.java
@@ -1,7 +1,6 @@
 package graphEditor;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import graph.*;
 import javafx.event.ActionEvent;
@@ -14,35 +13,51 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.QuadCurve;
+import javafx.scene.shape.*;
 
-enum EdgeDrawingSTates {DRAW_EDGE, NOT_DRAW_EDGE}
+enum EdgeDrawingStates {DRAW_EDGE, NOT_DRAW_EDGE}
 
 class EdgeVisual extends Group {
-    QuadCurve line;
+    Path line;
     TextField textWeigth;
 
     protected Button source;
     protected Button finish;
 
+    @Override 
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+
+        if (o instanceof EdgeVisual) {
+            EdgeVisual edgeObj = (EdgeVisual)o;
+            if (source.getLayoutX() == edgeObj.source.getLayoutX() && source.getLayoutY() 
+            == edgeObj.source.getLayoutY() && finish.getLayoutX() == edgeObj.finish.getLayoutX() && 
+            finish.getLayoutY() == edgeObj.finish.getLayoutY()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public EdgeVisual(Button start, Button end) {
         source = start;
         finish = end;
 
-        line = new QuadCurve();
+        line = new Path();
         textWeigth = new TextField("-1");
 
-        line.setControlX(0.0f);
-        line.setControlY(0.0f);
+        line.setStroke(Color.BLACK);
+        line.setFill(Color.TRANSPARENT);
 
-        line.setStartX(source.getLayoutX() + source.getBoundsInParent().getWidth() / 2.0);
-        line.setStartY(source.getLayoutY() + source.getBoundsInParent().getHeight() / 2.0);
-    
-        line.setEndX(finish.getLayoutX() + finish.getBoundsInParent().getWidth() / 2.0);
-        line.setEndY(finish.getLayoutY() + finish.getBoundsInParent().getHeight() / 2.0);
-
-        line.setViewOrder(1);
+        if (source.getLayoutX() == finish.getLayoutX() && finish.getLayoutY() == source.getLayoutY()) {
+            drawLoop();
+        } else {
+            drawEdge();
+        }
 
         setWeigthTextPosition();
 
@@ -50,11 +65,87 @@ class EdgeVisual extends Group {
         getChildren().add(textWeigth);
     }
 
+    private void drawLoop() {
+        double startX = source.getLayoutX() +  source.getBoundsInParent().getWidth() / 2.0;;
+        double startY = source.getLayoutY();
+
+        double endX = source.getLayoutX();
+        double endY = source.getLayoutY() + source.getBoundsInParent().getHeight() / 2.0;;
+
+        line.getElements().add(new MoveTo(startX, startY));
+
+        line.getElements().add(new ArcTo(25, 25, 0, endX, endY, true, false));
+
+        double sin = Math.sin(-1.5);
+        double cos = Math.cos(-1.5);
+
+        double x1 = (- 1.0 / 2.0 * cos + Math.sqrt(3) / 2 * sin) * 15.0 + endX;
+        double y1 = (- 1.0 / 2.0 * sin - Math.sqrt(3) / 2 * cos) * 15.0 + endY;
+
+        double x2 = (1.0 / 2.0 * cos + Math.sqrt(3) / 2 * sin) * 15.0 + endX;
+        double y2 = (1.0 / 2.0 * sin - Math.sqrt(3) / 2 * cos) * 15.0 + endY;
+
+        line.getElements().add(new LineTo(x1, y1));
+        line.getElements().add(new MoveTo(endX, endY));
+        line.getElements().add(new LineTo(x2, y2));
+    }
+
+    private void drawEdge() {
+        double startX = source.getLayoutX() + source.getBoundsInParent().getWidth() / 2.0;
+        double startY = source.getLayoutY() + source.getBoundsInParent().getHeight() / 2.0;
+
+        double endX = finish.getLayoutX() + finish.getBoundsInParent().getWidth() / 2.0;
+        double endY = finish.getLayoutY() + finish.getBoundsInParent().getHeight() / 2.0;        
+
+        double angle = Math.atan2((endY - startY), (endX - startX)); 
+        double sin = Math.sin(angle);
+        double cos = Math.cos(angle);
+
+        startX += source.getBoundsInParent().getWidth() / 2.0 * cos;
+        startY += source.getBoundsInParent().getWidth() / 2.0 * sin;
+
+        endX -= source.getBoundsInParent().getWidth() / 2.0 * cos;
+        endY -= source.getBoundsInParent().getWidth() / 2.0 * sin;
+
+        angle -= Math.PI / 2;
+        sin = Math.sin(angle);
+        cos = Math.cos(angle);
+
+        double x1 = (- 1.0 / 2.0 * cos + Math.sqrt(3) / 2 * sin) * 15.0 + endX;
+        double y1 = (- 1.0 / 2.0 * sin - Math.sqrt(3) / 2 * cos) * 15.0 + endY;
+
+        double x2 = (1.0 / 2.0 * cos + Math.sqrt(3) / 2 * sin) * 15.0 + endX;
+        double y2 = (1.0 / 2.0 * sin - Math.sqrt(3) / 2 * cos) * 15.0 + endY;
+
+        line.getElements().add(new MoveTo(startX, startY));
+        line.getElements().add(new LineTo(endX, endY));
+
+        line.getElements().add(new LineTo(x1, y1));
+        line.getElements().add(new MoveTo(endX, endY));
+        line.getElements().add(new LineTo(x2, y2));
+    }
+
     private void setWeigthTextPosition() {
-        double textStartX = Math.min(line.getStartX(), line.getEndX());
-        double textStartY = Math.min(line.getStartY(), line.getEndY());
-        double textX = Math.abs(line.getStartX() - line.getEndX()) / 2.0 + textStartX;
-        double textY = Math.abs(line.getStartY() - line.getEndY()) / 2.0 + textStartY;
+        double textX = 0.0;
+        double textY = 0.0;
+        if (source.getLayoutX() == finish.getLayoutX() && source.getLayoutY() == finish.getLayoutY()) {
+
+            textX = source.getLayoutX() - 60;
+            textY = source.getLayoutY() - 60;
+
+        } else {
+            double textOffset = 0.0;
+            if (source.getLayoutY() > finish.getLayoutY()) {
+                textOffset = 20;
+            } else {
+                textOffset = -20;
+            }
+            double textStartX = Math.min(source.getLayoutX(), finish.getLayoutX());
+            double textStartY = Math.min(source.getLayoutY(), finish.getLayoutY());
+    
+            textX = Math.abs(source.getLayoutX() - finish.getLayoutX()) / 2.0 + textStartX + textOffset;
+            textY = Math.abs(source.getLayoutY() - finish.getLayoutY()) / 2.0 + textStartY + textOffset;
+        }
 
         textWeigth.setMaxWidth(35);
         textWeigth.setLayoutX(textX);
@@ -90,7 +181,7 @@ public class GraphEditor implements IGraphEditor {
     private GraphicsContext context;
     private Pane parentBox;
 
-    private EdgeDrawingSTates edgeState;
+    private EdgeDrawingStates edgeState;
 
     private NodeVisual edgeStart;
     private NodeVisual edgeEnd;
@@ -102,16 +193,18 @@ public class GraphEditor implements IGraphEditor {
         context = canvas.getGraphicsContext2D();
         parentBox = (Pane)canvas.getParent();
         graphNodesCount = 0;
-        edgeState = EdgeDrawingSTates.NOT_DRAW_EDGE;
+        edgeState = EdgeDrawingStates.NOT_DRAW_EDGE;
 
         this.canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                context.setFill(Color.RED);
-                NodeVisual graphNode = createGraphNodeButton();
-                graphNode.setLayoutX(event.getX());
-                graphNode.setLayoutY(event.getY());
-                parentBox.getChildren().add(graphNode);
+                if (event.getButton().compareTo(MouseButton.PRIMARY) == 0) {
+                    NodeVisual graphNode = createGraphNodeButton();
+                    graphNode.setLayoutX(event.getX());
+                    graphNode.setLayoutY(event.getY());
+                    parentBox.getChildren().add(graphNode);
+                    edgeState = EdgeDrawingStates.NOT_DRAW_EDGE;
+                }
             }
         });
     }
@@ -125,11 +218,11 @@ public class GraphEditor implements IGraphEditor {
         graphNode.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                if (edgeState == EdgeDrawingSTates.NOT_DRAW_EDGE) {
-                    edgeState = EdgeDrawingSTates.DRAW_EDGE;
+                if (edgeState == EdgeDrawingStates.NOT_DRAW_EDGE) {
+                    edgeState = EdgeDrawingStates.DRAW_EDGE;
                     edgeDrawBegin(graphNode);
                 } else {
-                    edgeState = EdgeDrawingSTates.NOT_DRAW_EDGE;
+                    edgeState = EdgeDrawingStates.NOT_DRAW_EDGE;
                     edgeDrawEnd(graphNode);
                 }
             }
@@ -152,6 +245,21 @@ public class GraphEditor implements IGraphEditor {
         return graphNode;
     }
 
+    private EdgeVisual createEdge(Button start, Button end) {
+        EdgeVisual edge = new EdgeVisual(start, end);
+
+        edge.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton().compareTo(MouseButton.SECONDARY) == 0) {
+                    parentBox.getChildren().remove(edge);
+                }
+            }
+        });
+
+        return edge;
+    }
+
     private void edgeDrawBegin(NodeVisual clickedButton) {
         edgeStart = clickedButton;
     }
@@ -160,12 +268,15 @@ public class GraphEditor implements IGraphEditor {
         edgeEnd = clickedButton;
 
         
-        EdgeVisual edgeVis = new EdgeVisual(edgeStart, edgeEnd);
-        
-        edgeEnd.setEdge(edgeVis);
-        edgeStart.setEdge(edgeVis);
+        EdgeVisual edgeVis = createEdge(edgeStart, edgeEnd);
 
-        parentBox.getChildren().add(edgeVis);
+        if (!parentBox.getChildren().contains(edgeVis)) {
+            edgeEnd.setEdge(edgeVis);
+            edgeStart.setEdge(edgeVis);
+
+            parentBox.getChildren().add(edgeVis);
+            System.out.println("Add edge");
+        }
     }
 
     @Override
